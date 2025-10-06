36f8af7afa2ff5953b4251af12f6c4b46410e4a5
Max <max.dambron@efrei.net>
2025-10-06 11:01:31 +0200

    bibliobilibobiblioblbio

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -7,23 +7,23 @@ class Bibliotheque{
     List<Emprunt> Emprunts = new LinkedList<>();
 
     void emprunterLivre(int idLivre, Utilisateur u) throws LivreIndisponibleException{
-        if(Livres.get(idLivre).disponible) throw new LivreIndisponibleException("Ce livre n'est pas disponible");
-        Emprunts.add(new Emprunt(idLivre,u.id, LocalDate.now()));
-        Livres.get(idLivre).disponible = false;
+        if(Livres.get(idLivre).isDisponible()) throw new LivreIndisponibleException("Ce livre n'est pas disponible");
+        Emprunts.add(new Emprunt(LocalDate.now(),LocalDate.now().plusDays(15),idLivre,u));
+        Livres.get(idLivre).setDisponible(false);
     }
     void rendreLivre(int idLivre,Utilisateur u){
         for(Emprunt e : Emprunts){
-            if(e.idLivre == idLivre && e.uid=u.id){
+            if(e.getLivre().getId() == idLivre && e.getUtilisateur().getId()==u.getId()){
                 Emprunts.remove(e);
-                Livres.get(idLivre).disponible = true;
+                Livres.get(idLivre).setDisponible(true);
             }
         }
     }
     void afficheLivresDisponibles(){
         String lst = "Livres disponibles : ";
         for(Livre l: Livres){
-            if(l.disponible){
-                lst = lst + l.name + " ";
+            if(l.isDisponible()){
+                lst = lst + l.getTitre() + " ";
             }
         }
         System.out.println(lst);
