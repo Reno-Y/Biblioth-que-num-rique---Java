3f297d2bb8ce23baa96f8e6e358eaf22a07f4c25
Max <max.dambron@efrei.net>
2025-10-06 09:48:26 +0200

    bibliobilibobiblioblbio

---
+++ src/Bibliotheque.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Bibliotheque{
+    List<Livre> Livres = new ArrayList<>();
+    List<Utilisateur> Utilisateurs = new ArrayList<>() ;
+
+    void emprunterLivre(int idLivre, Utilisateur u){
+        if
+    }
+}
