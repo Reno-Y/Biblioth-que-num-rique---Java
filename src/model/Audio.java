129d52ea69b42215d5afc8d893fc3b473e828538
katchow <souhaib.sghaier@efrei.net>
2025-10-07 19:19:11 +0200

    Yiha

--- src/model/Audio.java
+++ src/model/Audio.java
@@ -1,9 +1,5 @@
 package model;
 
-/**
- * Record immuable représentant un média audio dans le catalogue
- * Étudiant A - Implémentation Media
- */
 public record Audio(int id, String titre, String artiste, String genre) implements Media {
 }
 
